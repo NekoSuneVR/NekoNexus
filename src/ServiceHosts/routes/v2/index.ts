094ef1a8e405efadc246e3c41ace1706095657bd
SniperGER <6178765+SniperGER@users.noreply.github.com>
2024-04-08 21:02:57 +0200

    Improved replication of Microsoft services (because reasons)

--- src/ServiceHosts/routes/v2/index.ts
+++ src/ServiceHosts/routes/v2/index.ts
@@ -30,6 +30,10 @@ router.use(new RegExp(`^/${ParadiseServiceSettings.WebServicePrefix}(.+)${Paradi
   const service = Services[req.params[0]];
   if (!service) return res.status(httpStatus.BAD_REQUEST).send('');
 
+  if (req.headers['content-type'] !== 'text/xml; charset=utf-8') {
+    res.statusMessage = `Cannot process the message because the content type '${req.headers['content-type']}' was not the expected type 'text/xml; charset=utf-8'.`;
+    return res.status(httpStatus.UNSUPPORTED_MEDIA_TYPE).send();
+  }
   if (!req.body || !Object.keys(req.body).length) return res.status(httpStatus.BAD_REQUEST).send('');
 
   if (!req.headers.soapaction) return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(SOAPResponse.createFault());
@@ -49,11 +53,31 @@ router.use(new RegExp(`^/${ParadiseServiceSettings.WebServicePrefix}(.+)${Paradi
     res.setHeader('Content-Type', 'text/xml; charset=utf-8');
     res.setHeader('Server', 'Microsoft-HTTPAPI/2.0');
 
+    if (!bytes) {
+      return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(SOAPResponse.createFault(req.headers.soapaction));
+    }
+
     return res.status(httpStatus.OK).send(SOAPResponse.create(bodyMethod, bytes ?? []));
   } catch (error: any) {
     console.error(error);
     return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
   }
 });
+
+router.use((req, res, next) => res.status(httpStatus.NOT_FOUND).send(`<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN""http://www.w3.org/TR/html4/strict.dtd">
+<HTML>
+
+<HEAD>
+\t<TITLE>Not Found</TITLE>
+\t<META HTTP-EQUIV="Content-Type" Content="text/html; charset=us-ascii">
+</HEAD>
+
+<BODY>
+\t<h2>Not Found</h2>
+\t<hr>
+\t<p>HTTP Error 404. The requested resource is not found.</p>
+</BODY>
+
+</HTML>`));
 
 export default router;
