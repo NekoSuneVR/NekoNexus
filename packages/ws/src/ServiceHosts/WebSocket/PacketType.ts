578aaa17a27287b427d7f8022ace6f1f7ef0739b
SniperGER <6178765+SniperGER@users.noreply.github.com>
2024-06-30 22:45:26 +0200

    Fix incorrent packet types (thanks, JavaScript)

--- packages/ws/src/ServiceHosts/WebSocket/PacketType.ts
+++ packages/ws/src/ServiceHosts/WebSocket/PacketType.ts
@@ -12,20 +12,20 @@ enum WebSocketPacketType {
   Error,
 
   ChatMessage = 1 << 10,
-  RoomChatMessage,
-  PlayerList,
-  PlayerJoined,
-  PlayerLeft,
-  RoomOpened,
-  RoomClosed,
-  PlayerJoinedRoom,
-  PlayerLeftRoom,
-  RoundStarted,
-  RoundEnded,
+  RoomChatMessage = (1 << 10) + 1,
+  PlayerList = (1 << 10) + 1,
+  PlayerJoined = (1 << 10) + 2,
+  PlayerLeft = (1 << 10) + 3,
+  RoomOpened = (1 << 10) + 4,
+  RoomClosed = (1 << 10) + 5,
+  PlayerJoinedRoom = (1 << 10) + 6,
+  PlayerLeftRoom = (1 << 10) + 7,
+  RoundStarted = (1 << 10) + 8,
+  RoundEnded = (1 << 10) + 9,
 
-  OpenRoom,
-  CloseRoom,
-  BanPlayer,
+  OpenRoom = (1 << 10) + 10,
+  CloseRoom = (1 << 10) + 11,
+  BanPlayer = (1 << 10) + 12,
 }
 
 export default WebSocketPacketType;
