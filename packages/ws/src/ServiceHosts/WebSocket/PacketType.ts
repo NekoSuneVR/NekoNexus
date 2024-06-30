98aaada56c08b0cc9ab59703f80ed3e832a1ef5b
SniperGER <6178765+SniperGER@users.noreply.github.com>
2024-06-30 22:54:26 +0200

    Fixed shifted lines

--- packages/ws/src/ServiceHosts/WebSocket/PacketType.ts
+++ packages/ws/src/ServiceHosts/WebSocket/PacketType.ts
@@ -13,19 +13,19 @@ enum WebSocketPacketType {
 
   ChatMessage = 1 << 10,
   RoomChatMessage = (1 << 10) + 1,
-  PlayerList = (1 << 10) + 1,
-  PlayerJoined = (1 << 10) + 2,
-  PlayerLeft = (1 << 10) + 3,
-  RoomOpened = (1 << 10) + 4,
-  RoomClosed = (1 << 10) + 5,
-  PlayerJoinedRoom = (1 << 10) + 6,
-  PlayerLeftRoom = (1 << 10) + 7,
-  RoundStarted = (1 << 10) + 8,
-  RoundEnded = (1 << 10) + 9,
+  PlayerList = (1 << 10) + 2,
+  PlayerJoined = (1 << 10) + 3,
+  PlayerLeft = (1 << 10) + 4,
+  RoomOpened = (1 << 10) + 5,
+  RoomClosed = (1 << 10) + 6,
+  PlayerJoinedRoom = (1 << 10) + 7,
+  PlayerLeftRoom = (1 << 10) + 8,
+  RoundStarted = (1 << 10) + 9,
+  RoundEnded = (1 << 10) + 10,
 
-  OpenRoom = (1 << 10) + 10,
-  CloseRoom = (1 << 10) + 11,
-  BanPlayer = (1 << 10) + 12,
+  OpenRoom = (1 << 10) + 11,
+  CloseRoom = (1 << 10) + 12,
+  BanPlayer = (1 << 10) + 13,
 }
 
 export default WebSocketPacketType;
